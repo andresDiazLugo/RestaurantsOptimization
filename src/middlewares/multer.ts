4f148816496bf81897a94178e210344f5d3f1a27
genjutsu <andresd.ad5@gmail.com>
2023-03-15 01:27:23 -0300

    changes in the file multer

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -7,19 +7,19 @@ import { destFiles } from '../utils/pathFiles.js'
 const storage = multer.diskStorage({
   destination: destFiles,
   filename: (_req, file, callback) => {
-    console.log("ejecutandome")
     callback(null, new Date().getTime().toString().concat(path.extname(file.originalname)))
   }
 })
 
 const upload = multer({
   storage,
-  // dest: destFiles,
+  dest: destFiles,
   // limits: {
   //   fileSize: (1000 * 1000) * 3
   // },
-  // fileFilter: (_req, file, callback) => {
-   
+  fileFilter: (_req, file, callback) => {
+
+  //  console.log(file)
   //   const imageTypes = /jpeg|jpg|png/
 
   //   const mineTypes = imageTypes.test(file.mimetype)
@@ -29,9 +29,13 @@ const upload = multer({
   //   if (mineTypes && extType) {
   //     callback(null, true)
   //   }
-
   //   callback(null, false)
-  // }
+  const formatFile = file.mimetype.split("/")[1].toLocaleLowerCase();
+    if (formatFile ==="jpeg" || formatFile ==="jpg" || formatFile === "png") {
+      callback(null, true)
+    }
+      callback(null, false)
+  }
 })
 
 export { upload }
