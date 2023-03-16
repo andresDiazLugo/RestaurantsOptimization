73cf2ecf19f444e64fc00d9e7ca402c6131e7cb4
Franco M <damianmaciel0@gmail.com>
2023-03-16 20:36:04 -0300

    Add user password reset

---
+++ src/utils/nodemailer.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer'
+import { type Mail } from '../interface/mail'
+
+export async function SendMail ({ to, subject, html }: Mail): Promise<any> {
+  const { USER_EMAIL, APP_PASS, USER_EMAIL_TO } = process.env
+
+  const mailOptions = {
+    to,
+    from: USER_EMAIL_TO,
+    subject,
+    text: 'Plaintext version of the message',
+    html
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: USER_EMAIL,
+      pass: APP_PASS
+    }
+  })
+
+  const nodemailerResponse = await transporter.sendMail(mailOptions)
+
+  return nodemailerResponse
+}
